242b940ca7789a2e5c89612751c311ae3d464f76
Rafacenter <166769380+Rafacenter32-2@users.noreply.github.com>
2024-04-11 22:12:06 -0300

    Add files via upload

--- post.py
+++ post.py
@@ -1,7 +1,17 @@
+conf = (open("config.conf")).read()
+title = False
+sep = False
+exec(conf)
 inpt = (open("input.txt"))
 x = inpt.read()
-y = input("titulo:")
+if (title):
+    y = input("titulo:")
+    print ("okay")
 x = x.replace("\n","<br>")
-out = "<h1>"+ y +"</h1> <p>"+ x +"</p>"
+if (title):
+    out = "<h1>"+ y +"</h1> <p>"+ x +"</p>"
+else:
+    out = "<p>"+ x +"</p>"
 w = open("output.txt", "w")
 w.write(out)
+print("Pronto")
